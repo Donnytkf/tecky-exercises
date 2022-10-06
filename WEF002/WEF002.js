efa1e84d714a2d50a46451416e645dc4c49a0dc6
Donny <donnytkf@gmail.com>
2022-10-06 10:33:12 +0800

    upload

---
+++ WEF002/WEF002.js
@@ -0,0 +1,11 @@
+const peter = {
+    name: "Peter",
+    age: 50,
+    students:[
+       { name:"Andy", age:20},
+       { name:"Bob", age:23},
+       {name: "Charlie", age:25 , exercises:[
+           { score: 60 , date: new Date("2019-01-05") }
+       ]}
+    ]
+}
